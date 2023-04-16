92a7fd81c88179bc7eb7c42369dc263f7692cb23
LoveSnowEx <78215884+LoveSnowEx@users.noreply.github.com>
2023-04-16 21:38:06 +0800

    Refactor ui with better using of tview (#4)

--- ui/ui.go
+++ ui/ui.go
@@ -2,81 +2,48 @@ package ui
 
 import (
 	"CPJudge/env"
-	"CPJudge/selector"
-	"fmt"
 	"os"
 	"os/exec"
 
-	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
+var (
+	app      *tview.Application
+	explorer *Explorer
+)
+
 func Run() {
-	selector, err := selector.NewSelector(env.OutputPath)
-	if err != nil {
-		panic(err)
-	}
+	contentView := NewContentView()
 
-	sidebar := NewSidebar(selector)
-	sidebar.Update()
-	contentView := NewContentView(selector)
-
-	grid := tview.NewGrid().
-		SetRows(0, 1).
-		SetColumns(22, 0).
-		SetBorders(true).
-		AddItem(NewHint(), 1, 0, 1, 2, 0, 0, false)
-
-	grid.AddItem(sidebar, 0, 0, 1, 1, 0, 0, false).
-		AddItem(contentView, 0, 1, 1, 1, 0, 0, false)
-
-	update := func() {
-		sidebar.Update()
-		contentView.Update()
-	}
-
-	app := tview.NewApplication().SetRoot(grid, true).SetFocus(grid)
-
-	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch {
-		case event.Rune() == 'q':
-			app.Stop()
-		case event.Key() == tcell.KeyLeft:
-			selector.PrevStu()
-			update()
-		case event.Key() == tcell.KeyRight:
-			selector.NextStu()
-			update()
-		case event.Key() == tcell.KeyUp:
-			selector.PrevFile()
-			update()
-		case event.Key() == tcell.KeyDown:
-			selector.NextFile()
-			update()
-		case event.Key() == tcell.KeyEnter:
-			app.Suspend(func() {
-				fmt.Println(selector.CurFileDir())
-				cmd := exec.Command("less", "-S", selector.CurFileDir())
-				cmd.Stdin = os.Stdin
-				cmd.Stdout = os.Stdout
-				cmd.Stderr = os.Stderr
-				cmd.Run()
-			})
-		}
-
-		return event
-	}).
-		SetMouseCapture(func(event *tcell.EventMouse, action tview.MouseAction) (*tcell.EventMouse, tview.MouseAction) {
-			switch {
-			case event.Buttons() == tcell.WheelUp:
-				selector.PrevStu()
-				update()
-			case event.Buttons() == tcell.WheelDown:
-				selector.NextStu()
-				update()
-			}
-			return event, action
+	explorer = NewExplorer(env.OutputPath)
+	explorer.SetChangedFunc(func(node *tview.TreeNode) {
+		path := node.GetReference().(string)
+		contentView.Load(path)
+	})
+	explorer.SetSelectedFunc(func(node *tview.TreeNode) {
+		path := node.GetReference().(string)
+		app.Suspend(func() {
+			cmd := exec.Command("less", "-S", path)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
 		})
+	})
+
+	main := tview.NewFlex().
+		AddItem(explorer, 24, 1, true).
+		AddItem(contentView, 0, 1, false)
+
+	root := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(main, 0, 1, true).
+		AddItem(NewHint(), 1, 1, false)
+
+	root.SetBorder(true)
+
+	app = tview.NewApplication().SetRoot(root, true).SetFocus(root)
 
 	if err := app.Run(); err != nil {
 		panic(err)
